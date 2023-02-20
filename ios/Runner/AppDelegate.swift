e53434557a2c386d2f536f0dfd0198b7c733fa4e
TonySilva7 <tony.deve7oper@gmail.com>
2023-02-20 15:52:40 -0300

    chore: config api key in AppDelegate on iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,21 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("SUA_CHAVE_API_AQUI")
+    // Pega as variáveis de ambiente definidas no launch.json
+    let dartDefinesString = Bundle.main.infoDictionary!["DART_DEFINES"] as! String
+    var dartDefinesDictionary = [String:String]()
+    for definedValue in dartDefinesString.components(separatedBy: ",") {
+      let decoded = String(data: Data(base64Encoded: definedValue)!, encoding: .utf8)!
+      let values = decoded.components(separatedBy: "=")
+
+      // print entry
+      
+      dartDefinesDictionary[values[0]] = values[1]
+    }
+
+    GMSServices.provideAPIKey(dartDefinesDictionary["GOOGLE_MAPS_API_KEY"]!)
+    // --- fim ---
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
